f765156cf1f75bf2b30b46a793a4783314ebb509
Ryoma Ishida <125534009+ryomappi@users.noreply.github.com>
2024-11-24 22:35:51 +0900

    remove: AddForceを削除

--- Assets/Scripts/Thruster.cs
+++ Assets/Scripts/Thruster.cs
@@ -5,7 +5,6 @@ public class Thruster : MonoBehaviour
     [Header("Thrust (N)"), SerializeField] public float force;  // スラスタの推力 (N = kg * m/s^2)
     public Vector3 direction { get; private set; }  // スラスタの向き
     [Header("Fuel Consumption (kg/s)")] public float fuelConsumption;  // 燃料消費量 (kg/s)
-    private Rigidbody satelliteRb;
 
     void Start()
     {
