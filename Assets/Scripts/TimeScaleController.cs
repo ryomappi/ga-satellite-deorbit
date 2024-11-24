b21c215848dc8bfa5d2408c178281e76bb419584
Ryoma Ishida <125534009+ryomappi@users.noreply.github.com>
2024-11-24 23:14:32 +0900

    fix: 0.1倍は必要ないので消した

--- Assets/Scripts/TimeScaleController.cs
+++ Assets/Scripts/TimeScaleController.cs
@@ -8,7 +8,7 @@ public class TimeScaleController : MonoBehaviour
     {
         // スライダーを描画
         GUI.Label(new Rect(10, 10, 200, 20), "Time Scale: " + timeScale.ToString("F2"));
-        timeScale = GUI.HorizontalSlider(new Rect(10, 40, 200, 20), timeScale, 0.1f, 100.0f);
+        timeScale = GUI.HorizontalSlider(new Rect(10, 40, 200, 20), timeScale, 1f, 100.0f);
 
         // TimeScale を更新
         Time.timeScale = timeScale;
